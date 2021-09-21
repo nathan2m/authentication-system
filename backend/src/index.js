a1e728eaad958c07ada3bbc66d2398b0928189af
nathan2m <nathan.manera@gmail.com>
2021-09-21 16:24:38 -0300

    Backend
    Added cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,12 @@
-const PORT = 3000;
+const PORT = 4000;
 const address = "localhost";
 
 const express = require('express');
 const bodyParser = require("body-parser");
-
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
